7f2e11dfe3d3c03ea4082c8964c43a580b7a29f8
吴文亮 <wuwenliang@ACA8016D.ipt.aol.com>
2017-05-18 10:37:07 +0800

    ModuleB

---
+++ src/main/java/com/cdsf/yzy/moduleb/ModuleBActivity.java
@@ -0,0 +1,12 @@
+package com.cdsf.yzy.moduleb;
+
+import android.app.Activity;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+
+/**
+ * Created by wuwenliang on 2017/5/18.
+ */
+@Route(path="module/moduleB")
+public class ModuleBActivity extends Activity {
+}
